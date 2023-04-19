9b96d5058c6a6d5f7013c8d223ff90c04bddb2fd
FangFoe <fang-pa@hotmail.com>
2023-04-19 20:57:23 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
-import VCalendar from 'v-calendar';
-import './axios'
+// import VCalendar from 'v-calendar';
 
-createApp(App).use(router).use(VCalendar, {}).mount('#app')
+import './axios'
+// .use(VCalendar, {})
+createApp(App).use(router).mount('#app')
